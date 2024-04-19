c8fa5bc5188c2aea866466087265d8d17710bc78
Darshula <tanmayvijaykulkarni2002@gmail.com>
2024-04-19 21:32:33 +0530

    Moved logic to class

--- tech_interview.py
+++ tech_interview.py
@@ -1,39 +1,19 @@
 import json
-import re
-
-import pypdf
-
-with open('Path for resume', 'rb') as file:
-    pdf_reader = pypdf.PdfReader(file)
-    text = ''
-    for page_num in range(len(pdf_reader.pages)):
-        page = pdf_reader.pages[page_num]
-        text += page.extract_text()
-
-question_database = {}
-
-with open('questions.json') as questions:
-    question_database = json.load(questions)
 
 
-def extract_skills(text):
-    skills = set(re.findall(r'\b\w+\b', text.lower()))
-    return skills
+class TechnicalInterviewer:
+    def __init__(self) -> None:
+        with open('questions.json') as questions:
+            self.question_database = json.load(questions)
+
+    def generate_questions(self, skills):
+        skills = set(skills)
+        questions = []
+        for skill in skills:
+            if skill in self.question_database:
+                questions.extend(self.question_database[skill])
+        return questions
 
 
-def generate_questions(skills):
-    questions = []
-    for skill in skills:
-        if skill in question_database:
-            questions.extend(question_database[skill])
-    return questions
-
-
-print(question_database)
-
-skills = extract_skills(text)
-questions = generate_questions(skills)
-
-print("\nGenerated Interview Questions:")
-for question in questions:
-    print("- " + question)
+if __name__ == '__main__':
+    ti = TechnicalInterviewer()
